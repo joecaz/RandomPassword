da53cb8de5350ec3942c8455ffbd63911db0543e
Joseph Cicinelli <jcicinelli@gmail.com>
2017-05-28 16:42:30 -0700

    Initial Commit

---
+++ RandomPassword/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by joecaz on 5/28/17.
+//  Copyright © 2017 Coyote Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomPassword/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomPassword
+//
+//  Created by joecaz on 5/28/17.
+//  Copyright © 2017 Coyote Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
